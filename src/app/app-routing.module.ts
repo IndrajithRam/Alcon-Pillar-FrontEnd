6b5c474eea2df27a224e75853ac39d8ce1ef8816
IndrajithRam <indrajithcool@yahoo.com>
2023-04-13 11:05:13 -0400

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,40 +1,61 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AddTournamentComponent } from './views/add-tournament/add-tournament.component';
-import { EditTournamentComponent } from './views/edit-tournament/edit-tournament.component';
-import { HomeComponent } from './views/home/home.component';
-import { TournamentComponent } from './views/tournament/tournament.component';
-import { LoginComponent } from './views/login/login.component';
-import { RegisterComponent } from './views/register/register.component';
-import { AuthGuard } from './services/auth.guard';
-import { ErrorComponent } from './views/error/error.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AddTournamentComponent } from "./views/add-tournament/add-tournament.component";
+import { EditTournamentComponent } from "./views/edit-tournament/edit-tournament.component";
+import { HomeComponent } from "./views/home/home.component";
+import { TournamentComponent } from "./views/tournament/tournament.component";
+import { LoginComponent } from "./views/login/login.component";
+import { RegisterComponent } from "./views/register/register.component";
+import { AuthGuard } from "./services/auth.guard";
+import { ErrorComponent } from "./views/error/error.component";
 
 const routes: Routes = [
   {
-    path: 'home',
-    component: HomeComponent, data: { title: 'Home' },
+    path: "home",
+    component: HomeComponent,
+    data: { title: "Home" },
   },
   {
-    path: '',
-    component: HomeComponent, data: { title: 'Home' },
+    path: "",
+    component: HomeComponent,
+    data: { title: "Home" },
   },
   {
-    path: 'error',
-    component: ErrorComponent
+    path: "error",
+    component: ErrorComponent,
   },
-  { path: 'tournamentsList', component: TournamentComponent, data: { title: 'Tournament' } },
-  { path: 'displayAddTournament', component: AddTournamentComponent, data: { title: 'Add Tournament' }, canActivate: [AuthGuard] },
+  {
+    path: "tournamentsList",
+    component: TournamentComponent,
+    data: { title: "Tournament" },
+  },
+  {
+    path: "displayAddTournament",
+    component: AddTournamentComponent,
+    data: { title: "Add Tournament" },
+    canActivate: [AuthGuard],
+  },
   // { path: 'displayEditTournament', component: EditTournamentComponent, data: { title: 'edit Tournament' } },
-  { path: 'tournamentsList/:mode', component: EditTournamentComponent, canActivate: [AuthGuard] },
-  { path: 'tournamentsList/edit/:id', component: EditTournamentComponent, canActivate: [AuthGuard] },
-  { path: 'tournamentsList', component: TournamentComponent },
-  { path: 'login', component: LoginComponent, data: { title: 'Login' } },
-  { path: 'register', component: RegisterComponent, data: { title: 'Register' } },
-  
+  {
+    path: "tournamentsList/:mode",
+    component: EditTournamentComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: "tournamentsList/edit/:id",
+    component: EditTournamentComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: "login", component: LoginComponent, data: { title: "Login" } },
+  {
+    path: "register",
+    component: RegisterComponent,
+    data: { title: "Register" },
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
